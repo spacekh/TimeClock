6c84f62576dc814dfaa60b7e950a87c3186cf8d8
Kristen Hammack <kristen.hammack@cbre.com>
2018-04-17 08:17:58 -0500

    v1.1 -- fixes bug where timesheet might not be created; fixes bug where timesheet was not System.NewLine-separated; adds "open timesheet" button

--- timesheet/Form2.cs
+++ timesheet/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -14,20 +15,33 @@ namespace FolderSweeper
     public partial class Form2 : Form
     {
         DateTime timein, timeout;
-        string path = "C:\\Users\\Public\\TestFolder\\timesheet.txt";
+        string dir = "C:\\Users\\Public\\TestFolder\\";
+        string path;
         public Form2()
         {
+            path = dir + "timesheet.txt";
             InitializeComponent();
+            Text = "Time Clock App";
+            Icon = timesheet.Properties.Resources.ClockIcon;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             timeout = DateTime.Now;
             label2.Text = timeout.ToShortTimeString();
-            string s = $"date: {DateTime.Today.ToShortDateString()}; \tin: {timein.ToShortTimeString()}; \tout: {timeout.ToShortTimeString()}; \telapsed: {(timeout - timein).TotalHours}";
+            string s = $"date: {DateTime.Today.ToShortDateString()}; \tin: {timein.ToShortTimeString()}; \tout: {timeout.ToShortTimeString()}; \telapsed: {(timeout - timein).TotalHours}{Environment.NewLine}";
             if (File.Exists(path)) File.AppendAllText(path, s);
-            else File.WriteAllText(path, s);
+            else
+            {
+                Directory.CreateDirectory(dir);
+                File.WriteAllText(path, s);
+            }
             
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start(path);
         }
 
         private void button1_Click(object sender, EventArgs e)
